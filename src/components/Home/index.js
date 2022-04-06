6b798d4e8e95770078b6f0e8da37d8cc7b3e3418
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-04-05 22:01:52 -0700

    added Home component to the react router dom

---
+++ src/components/Home/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,10 +2,20 @@ import './index.scss'
 import React from 'react'
 import { Sidebar } from '../Sidebar'
 
+
 export const Layout = () => {
   return (
-    <div>
+    <div className="App">
       <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        {/* <Outlet /> */}
+        <span className="tags bottom-tags">
+          &lt;body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }

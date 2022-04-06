204b3cf74ce3ee296f1d6743a015d7d633bb1f27
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-04-05 17:56:26 -0700

    added icons and styled them in the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,43 @@
 import './index.scss'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
-// import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+// import { faUser } from '@fortawesome/free-solid-svg-icons'
+// import { GiLetterBomb } from '@fortawesome/free-solid-svg-icons'
+// import { FaHeart } from 'react-icons/fa'
 
 export const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        {/* <img src={LogoSubtitle} alt="slobodan" /> */}
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
       </Link>
+
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#FF5733" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#FF5733" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#FF5733" />
+        </NavLink>
+      </nav>
     </div>
   )
 }
